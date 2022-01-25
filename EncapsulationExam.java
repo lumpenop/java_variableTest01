7426e585fef1108fb927efaaf04956105e63a457
lumpenop <swk5373@gmail.com>
2022-01-25 10:22:42 +0900

    변수명 소문자로 변경, 초기 설정 기능 추가

--- EncapsulationExam.java
+++ EncapsulationExam.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 class EncapsulationExam{
 
     private int weight = 0;
-    private String password = "1234";
+    private String password = "";
 
     private void getWeight(String password){
-        if(checkPass(password)){
+        if(isRightPass(password)){
             System.out.println("몸무게는 " + this.weight + "kg 입니다.");
         }else{
             System.out.println("비밀번호가 틀렸습니다.");
@@ -14,25 +14,25 @@ class EncapsulationExam{
     }
 
     private void setWeight(String password, int weight){
-        if(checkPass(password)){
+        if(isRightPass(password)){
             this.weight = weight;
-            System.out.println("몸무게가 " + this.weight + "kg로 변경되었습니다.");
+            System.out.println("몸무게가 " + this.weight + "kg로 설정되었습니다.");
         }else{
             System.out.println("비밀번호가 틀렸습니다.");
         }
     }
 
-    private void changePass(String password, String newPassword){
+    private void setPass(String password, String newPassword){
         
-        if(checkPass(password)){
+        if(isRightPass(password)){
             this.password = newPassword;
-            System.out.println("비밀번호가 변경되었습니다.");
+            System.out.println("비밀번호가 설정되었습니다.");
         }else{
             System.out.println("비밀번호가 틀렸습니다.");
         }
     }
 
-    private boolean checkPass(String password){
+    private boolean isRightPass(String password){
         if(password.equals(this.password)){
             return true;
         }else{
@@ -42,8 +42,15 @@ class EncapsulationExam{
 
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        EncapsulationExam EE = new EncapsulationExam();
+        EncapsulationExam ee = new EncapsulationExam();
         
+        System.out.print("비밀번호 > ");
+        String firstPass = scanner.nextLine();
+        ee.setPass("", firstPass);
+        System.out.print("몸무게 > ");
+        int firstWeight = Integer.parseInt(scanner.nextLine());
+        ee.setWeight(firstPass, firstWeight);
+
         while(true){
             System.out.println("\n1. 몸무게 검색 2. 몸무게 변경 3.비밀번호 변경 4. 종료");
             System.out.print(">> ");
@@ -53,21 +60,21 @@ class EncapsulationExam{
                 System.out.println("몸무게 검색을 위해 비밀번호를 입력해주세요.");
                 System.out.print("비밀번호 > ");
                 String password = scanner.nextLine();
-                EE.getWeight(password);
+                ee.getWeight(password);
             }else if(menu==2){
                 System.out.println("몸무게 변경을 위해 비밀번호와 변경할 몸무게를를 입력해주세요.");
                 System.out.print("비밀번호 > ");
                 String password = scanner.nextLine();
                 System.out.print("몸무게 > ");
                 int weight = Integer.parseInt(scanner.nextLine());
-                EE.setWeight(password, weight);
+                ee.setWeight(password, weight);
             }else if(menu==3){
                 System.out.println("비밀번호 변경을 위해 현재 비밀번호와 변경할 비밀번호를 입력해주세요.");
                 System.out.print("현재 비밀번호 > ");
                 String password = scanner.nextLine();
                 System.out.print("변경할 비밀번호 > ");
                 String newPass = scanner.nextLine();
-                EE.changePass(password, newPass);
+                ee.setPass(password, newPass);
             }else{   
                 System.exit(1); 
             }
@@ -76,6 +83,6 @@ class EncapsulationExam{
     } // main
 
     static{
-        System.out.println("\n몸무게 관리 프로그램");
+        System.out.println("\n초기 비밀번호와 몸무게를 설정해주세요");
     }
 }
